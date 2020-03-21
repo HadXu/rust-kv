d40183645d914e658a65a04ad010e0cf5d241843
hadxu <hadxu123@gmail.com>
2020-03-21 16:23:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# A simple KV Database with Rust
+
+## Project 1: The Rust toolbox
+## Project 2: Log-structured file I/O
+## Project 3: Synchronous client-server networking
+## Project 4: Concurrency and parallelism
+## Project 5: Asynchronous programming in Rust
+
+
+
+## cite
+- [talent-plan-rust](https://github.com/pingcap/talent-plan/tree/master/rust)
+- [Write a Good CLI Program](https://qiita.com/tigercosmos/items/678f39b1209e60843cc3)
+- [kvs](https://gitlab.com/aknudsen/bitcask-key-value-store/-/blob/module-1/src/bin/kvs.rs)
+- [practical-networked-applications-in-rust](https://arveknudsen.com/posts/practical-networked-applications-in-rust/module-1/)
+- [kv](https://docs.rs/kv/0.20.2/kv/)
+- [clap](https://github.com/clap-rs/clap)
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+extern crate clap;
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("KV")
+        .version("1.0")
+        .author("hadxu123@gmail.com")
+        .about("KV Store")
+        .subcommand(
+            App::new("get")
+                .about("gets a key")
+                .arg(Arg::with_name("key").required(true).help("Key to get")),
+        )
+        .subcommand(
+            App::new("set")
+                .about("sets a key")
+                .arg(Arg::with_name("key").required(true).help("Key to set"))
+                .arg(Arg::with_name("value").required(true).help("value to set")),
+        )
+        .subcommand(
+            App::new("rm")
+                .about("remove key")
+                .arg(Arg::with_name("key").required(true).help("Key to remove")),
+        )
+        .get_matches();
+
+
+    match matches.subcommand_name() {
+        Some("get") => {
+            let sub_matches = matches.subcommand_matches("get").unwrap();
+            let key = sub_matches.value_of("key").unwrap();
+            println!("get {}", key);
+        }
+        Some("set") => {
+            let sub_matches = matches.subcommand_matches("set").unwrap();
+            let key = sub_matches.value_of("key").unwrap();
+            let value = sub_matches.value_of("value").unwrap();
+            println!("set {}-{}", key, value);
+        }
+        Some("rm") => {
+            let sub_matches = matches.subcommand_matches("rm").unwrap();
+            let key = sub_matches.value_of("key").unwrap();
+            println!("rm {}", key);
+        }
+        _ => panic!("Unrecognized subcommand"),
+    }
+}
